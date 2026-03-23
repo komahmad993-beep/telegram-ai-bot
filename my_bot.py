e1f9f292ba5c3c49c0fefbdbbfdfb6e20e85dbc8
komahmad993-beep <kom.ahmad993@gmail.com>
2026-03-23 09:23:14 +0500

    Update my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -6,22 +6,37 @@ import os
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
-user_limits = {}
+user_data = {}
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("🚀 Salom! Men @InspiRocketBot")
 
+from datetime import datetime
+
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.message.from_user.id
     user_text = update.message.text
+    
+    print(f"{user_id}: (@{username}): {user_text}")
 
-    # limitni tekshiramiz
-    if user_id not in user_limits:
-        user_limits[user_id] = 5
+    today = datetime.now().date()
 
-    if user_limits[user_id] <= 0:
+    # user yo‘q bo‘lsa
+    if user_id not in user_data:
+        user_data[user_id] = {
+            "date": today,
+            "limit": 15
+        }
+
+    # yangi kun bo‘lsa reset
+    if user_data[user_id]["date"] != today:
+        user_data[user_id]["date"] = today
+        user_data[user_id]["limit"] = 15
+
+    # limit tugagan bo‘lsa
+    if user_data[user_id]["limit"] <= 0:
         await update.message.reply_text(
-            "🚫 Sizning bepul limit tugadi.\nPremium olish uchun yozing 💰"
+            "🚫 Kunlik limit tugadi (15 ta).\n💰 Premium olish uchun yozing."
         )
         return
 
@@ -35,16 +50,15 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         bot_reply = response.choices[0].message.content
 
-        # faqat muvaffaqiyatli bo‘lsa kamayadi
-        user_limits[user_id] -= 1
+        # kamaytiramiz
+        user_data[user_id]["limit"] -= 1
 
     except Exception as e:
         bot_reply = f"Xatolik: {e}"
 
     await update.message.reply_text(
-        f"{bot_reply}\n\n🧠 Qoldi: {user_limits[user_id]}"
+        f"{bot_reply}\n\n🧠 Qoldi: {user_data[user_id]['limit']}/15"
     )
-
 app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
